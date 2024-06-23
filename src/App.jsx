43e2796d60733a1fe6fd035aecf3e9fc4dc2cade
Roxaneryz <roxane5666@yahoo.gr>
2024-06-23 10:53:09 +0300

    making routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import { Heading } from 'components';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Header } from "./components";
+import { lazy } from "react";
 
+const Home = lazy(() =>import("pages/Home.jsx") )
+const Rates = lazy(()=>import("pages/Rates"))
 export const App = () => {
-  return <Heading title="Just do it!" />;
+
+
+
+  return (
+  <Routes>
+    <Route path="/" element={<Header/>}>
+        <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="*" element = {<Navigate to="/"/>}/>
+    </Route>
+  </Routes>
+
+  )
+
 };
