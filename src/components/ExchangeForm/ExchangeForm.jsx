68e6d41cfebceb3b24521023b6e1060f1552b7fd
Anastasiiya Onyshchuk <shapoval.anastasiiya@gmail.com>
2024-06-23 13:07:11 +0300

    add task 3

--- src/components/ExchangeForm/ExchangeForm.jsx
+++ src/components/ExchangeForm/ExchangeForm.jsx
@@ -1,14 +1,33 @@
 import { RiExchangeDollarFill } from 'react-icons/ri';
 import styles from './ExchangeForm.module.css';
+import { useDispatch } from 'react-redux';
+import { fetchExchangeCurrency } from '../../redux/currency/operations';
 
 export const ExchangeForm = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const { value } = e.target.elements.currency;
+    const [amount, from, , to] = value.split(' ');
+    dispatch(fetchExchangeCurrency({amount, from, to}));
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <button className={styles.button} type="submit">
         <RiExchangeDollarFill className={styles.icon} />
       </button>
 
-      <input title="Request format 15 USD in UAH" className={styles.input} />
+      <input
+        name="currency"
+        type="text"
+        pattern="^\d+(\.\d{1,2})?\s[a-zA-Z]{3}\sin\s[a-zA-Z]{3}$"
+        placeholder="15 USD in UAH"
+        title="Request format 15 USD in UAH"
+        className={styles.input}
+        required
+      />
     </form>
   );
 };
