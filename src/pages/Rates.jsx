3aca1ac9f9b68e3d73f51b5aedcc0493d44d429d
Olga Kondratenko <kondratenko666olga@gmail.com>
2024-07-07 11:21:22 +0300

    filter

--- src/pages/Rates.jsx
+++ src/pages/Rates.jsx
@@ -1,10 +1,18 @@
 import { Wave } from 'react-animated-text';
 
-import { Container, Heading, Section } from 'components';
+import { Container, Filter, Heading, Loader, RatesList, Section } from 'components';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectBaseCurrency, selectError, selectLoading, selectRates } from '../redux/selectors';
+import { useEffect } from 'react';
+import { fetchLatestSymbols } from '../redux/currency/operations';
 
 const Rates = () => {
-  const isError = false;
-
+  const isError = useSelector(selectError);
+  const isLoading = useSelector(selectLoading);
+  const rates = useSelector(selectRates);
+  const baseCurrency = useSelector(selectBaseCurrency);
+const dispach = useDispatch();
+useEffect(()=>{dispach(fetchLatestSymbols(baseCurrency))}, [baseCurrency, dispach]);
   return (
     <Section>
       <Container>
@@ -19,13 +27,16 @@ const Rates = () => {
             />
           }
         />
-
+        {rates.length > 0 && <Filter/>}
+        <RatesList rates={rates}/>
+{isLoading && (<Loader/>)}
         {isError && (
           <Heading
             error
             title="Something went wrong...😐 We cannot show current rates!"
           />
         )}
+        
       </Container>
     </Section>
   );
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,3 +2,4 @@ export const selectBaseCurrency = state => state.currency.baseCurrency;
 export const selectLoading = state => state.currency.isLoading;
 export const selectError = state => state.currency.error;
 export const selectExchangeInfo = state => state.currency.exchangeInfo;
+export const selectRates = state => state.currency.rates;
